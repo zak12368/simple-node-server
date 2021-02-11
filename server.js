ccfa734ff4e8dc7068faa6de9eff545e6fb1c2f9
Zakaria Boussoffara <zakaria.boussoffara@polymtl.ca>
2021-02-11 16:34:27 -0500

    final commit, server deployed on azure

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
+http = require("http");
+WebSocket = require("ws");
 const port = process.env.PORT || 4005;
-let runningMessage = 'Server is running on port ' + port;
+let runningMessage = 'Websocket server started on port ' + port;
+server = http.createServer(app)
+websocketServer = new WebSocket.Server({ server });
 
 app.get('/', (req, res) => {
   console.log('API was successfully requested');
   res.send(runningMessage);
 });
 
-const server = app.listen(port, () => {
+//when a websocket connection is established
+websocketServer.on('connection', (webSocketClient) => {
+  //send feedback to the incoming connection
+  webSocketClient.send('{ "connection" : "ok"}');
+
+  //when a message is received
+  webSocketClient.on('message', (message) => {
+
+      //for each websocket client
+      websocketServer
+          .clients
+          .forEach(client => {
+              //send the client the current message
+              client.send(`{ "message" : ${message} }`);
+          });
+  });
+});
+
+server.listen(port, () => {
   console.log(runningMessage);
 });
 
