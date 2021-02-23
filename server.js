9deed767992c489948fb964d79adac1a8965381c
Santiago <santi_t9707@hotmail.com>
2021-02-22 22:39:14 -0500

    socket.io and rooms implemented

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
+const { addUser, removeUser, getUser, getUserInRoom } = require('./services/connectionManager')
+const { generatemsg } = require('./services/msgGenerator')
+
 const express = require('express');
 const app = express();
 http = require("http");
-WebSocket = require("ws");
 const port = process.env.PORT || 4005;
 let runningMessage = 'Websocket server started on port ' + port;
-server = http.createServer(app)
-websocketServer = new WebSocket.Server({ server });
+server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "http://localhost:4200",
+    }
+});
+
+
 var cors = require('cors');
 
 const bodyParser = require('body-parser')
@@ -13,39 +22,61 @@ const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 )
 
 app.use(cors({
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'x-access-token', 'XSRF-TOKEN'],
-  preflightContinue: false
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'x-access-token', 'XSRF-TOKEN'],
+    preflightContinue: false
 }))
 
 app.get('/', (req, res) => {
-  console.log('API was successfully requested');
-  res.send(runningMessage);
+    console.log('API was successfully requested');
+    res.send(runningMessage);
 });
 
 //when a websocket connection is established
-websocketServer.on('connection', (webSocketClient) => {
-  //send feedback to the incoming connection
-  webSocketClient.send('{ "connection" : "ok"}');
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on("joinRoom", ({ username, room }) => {
+        addUser({ id: socket.id, username, room })
 
-  //when a message is received
-  webSocketClient.on('message', (message) => {
+        socket.join(room)
+            // socket.emit("message", generatemsg("Admin ,Welcome"))
+            // socket.broadcast.to(room).emit("message", `Admin ${user.username} has joined!`);
 
-      //for each websocket client
-      websocketServer
-          .clients
-          .forEach(client => {
-              //send the client the current message
-              client.send(`${message}`);
-          });
-  });
+
+
+
+    })
+
+    socket.on("sendMessage", (msg) => {
+        let mess = JSON.parse(msg);
+        if (mess.roomId) {
+            console.log("room:", mess.roomId, "msg:", mess.message);
+            io.to(mess.roomId).emit("message", msg)
+        }
+    })
+
+    socket.on("leaveRoom", (username, room) => {
+        const user = removeUser(username, room)
+            // console.log(user, 'diconnected')
+            // if (user) {
+            //     io.to(room).emit("message", generatemsg(`Admin ${user.username} A user  has left`))
+            // }
+
+    })
+
+    socket.on("disconnect", () => {
+        const user = removeUser(username, 'Clobal');
+    });
 });
+
+
+
 
 app.get('/users', db.getUsers)
 app.get('/users/:username', db.getUserByUsername)
@@ -60,7 +91,7 @@ app.get('/connectedUsers/:username', db.getConnectedUser)
 app.delete('/connectedSockets/:socketId', db.deleteBySocket)
 
 server.listen(port, () => {
-  console.log(runningMessage);
+    console.log(runningMessage);
 });
 
 module.exports = server;
---
+++ services/connectionManager.js
@@ -0,0 +1,64 @@
+const connections = [];
+const addUser = ({ id, username, room }) => {
+
+    // //vlidate data
+    // if (!username || !room) {
+    //     return {
+    //         error: 'Username and room are required!'
+    //     }
+    // }
+
+    // //check for existing users
+    // const existingUser = connections.find((user) => {
+    //     return user.room == room && user.username == username;
+    // })
+
+    // //va;idate username
+    // if (existingUser) {
+    //     return {
+    //         error: "username iss already used"
+    //     }
+    // }
+
+    //store user
+    const user = { id, username, room };
+    connections.push(user);
+    console.log(connections);
+    return { user };
+}
+
+
+const removeUser = (username, room) => {
+    const index = connections.findIndex((user) => {
+        return (user.username === username && user.room === room);
+    })
+    if (index !== -1) {
+        console.log(connections);
+        return connections.splice(index, 1)[0];
+    }
+
+}
+
+const getUser = (id) => {
+    return connections.find((user) => {
+        return user.id === id;
+    })
+}
+
+const getUserInRoom = (room) => {
+    const u = [];
+    connections.filter((user) => {
+
+        user.room === room;
+    })
+    console.log(connections);
+    return connections;
+
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
