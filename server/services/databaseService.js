3841ff14a26befa2a5215d1cdc8c49b3b2102e41
Zakaria Boussoffara <zakaria.boussoffara@polymtl.ca>
2021-02-24 19:28:36 -0500

    Update databaseService.js

--- server/services/databaseService.js
+++ server/services/databaseService.js
@@ -7,12 +7,14 @@ const pool = new Pool({
   // database: 'projet 3',
   // password: 'postgrespw',
 
-  host: 'exotik-db.postgres.database.azure.com',
-  database: "postgres",
-  user: 'admin_exotik@exotik-db',
-  password: 'T79?2m>(CkdMqC5N',
-
-  port: 5432
+  host: 'ec2-3-221-49-44.compute-1.amazonaws.com',
+  database: "dfqdc5eh5tnqp",
+  user: 'ucwvuojbdwhurn',
+  password: 'a3783d051846cad2b94e755727ed73c9921696f6ece0d22c9960e19b9889664f',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM account ORDER BY account_username ASC', (error, results) => {
